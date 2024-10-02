83e8bfd3ad12cc44e6fa93ab8c43691485b36617
PepsiMonster <mikecelestialfiend@gmail.com>
2024-10-02 17:13:14 +0300

    Co-authored-by: Супонина Анастасия Павловна <SuponinaAP@users.noreply.github.com> Co-authored-by: VsevolodNirdoshi <VsevolodNirdoshi@users.noreply.github.com>

--- NewFile.py
+++ NewFile.py
@@ -1,5 +1,13 @@
 import numpy as np
-A = int(input("Input first number "))
-B = int(input("Input second number "))
-C = A+B*2
-print(C)
+L = int(input())
+v1 = int(input())
+v2 = int(input())
+vm = int(input())
+#v2_vm = v2+vm #скорость сближения второго поезда и мухи
+#v1_vm = v1+vm #скорость сближения первого поезда и мухи
+v_crush = v1+v2 #скорость сблиэения 2 поездов
+t = L/v_crush
+#v_comb = v_crush+vm
+num = t*vm
+#t_fin = L/v_comb
+print(int(num)) 
